4fd2afe2f9ac0b0388add901c39b1d500ecc5481
Your Name <your_email@whatever.com>
2020-04-10 01:06:36 +0400

    End video 6

--- sandbox/src/main/java/MyProgramm.java
+++
@@ -1,7 +0,0 @@
-public class MyProgramm{
-	public static void main (String[] args)
-	{
-		System.out.print("HellosdfsdWorld");
-	
-}
-}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MP.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+public class MP {
+	public static void main (String[] args)
+	{
+		System.out.print("Hello, my name is Artem1!");
+	
+}
+}
