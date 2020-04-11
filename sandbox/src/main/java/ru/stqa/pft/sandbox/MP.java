dd6c9e55b393cb6880c2411fb2b5c96c761c6bcd
Your Name <your_email@whatever.com>
2020-04-11 19:15:39 +0400

    Lecture 8

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MP.java
@@ -1,9 +1,8 @@
 package ru.stqa.pft.sandbox;
 
 public class MP {
-	public static void main (String[] args)
-	{
-		System.out.print("Hello, my name is Artem1!");
-	
-}
+    public static void main(String[] args) {
+        System.out.print("Hello, my name is Artem1!");
+
+    }
 }
