4f1018c124ed680d56a1d947b0602a935f126762
Your Name <your_email@whatever.com>
2020-04-16 15:19:35 +0400

    1 блок лекций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MP.java
@@ -2,7 +2,18 @@ package ru.stqa.pft.sandbox;
 
 public class MP {
     public static void main(String[] args) {
-        System.out.print("Hello, my name is Artem1!");
+
+        Square s = new Square(5);
+        //s.l = 5;
+        System.out.println(s.l + " " + s.area());
+
+
+
+        Rectangle r = new Rectangle(4,6);
+        //r.a =4;
+        //r.b =6;
+        System.out.println(r.a + " " + r.b + " " + r.area());
 
     }
 }
+
